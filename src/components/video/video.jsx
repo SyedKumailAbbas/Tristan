53f279ced9b44a44713fa51de6ec9904951e422d
Punoo-Khan <111001490+Punoo-Khan@users.noreply.github.com>
2025-08-17 15:03:54 +0500

    added border over the video.jsx

--- src/components/video/video.jsx
+++ src/components/video/video.jsx
@@ -1,7 +1,7 @@
 import "./video.css";
 import bgVideo from "../../assets/Main Video.mp4";
 import playIcon from "../../assets/noto_play-button.svg";
-
+import Border from "../../assets/Group 100.svg"
 export default function Hero() {
   return (
     <div className="relative video-container">
@@ -10,6 +10,13 @@ export default function Hero() {
         Your browser does not support the video tag.
       </video>
       
+      {/* Frame overlay around video */}
+      <img
+        src={Border}
+        alt="Video Frame"
+        className="absolute inset-0 w-full h-full object-cover z-10 pointer-events-none transform scale-[1.01]"
+      />
+
       {/* Button Over the Video */}
       <button className="absolute top-[553px] left-[101px] px-6 py-2 rounded-full bg-[#6E530C] text-white flex items-center gap-3 shadow-[0px_4px_4px_0px_#FFBD52] hover:cursor-pointer focus:outline-none " >
         <span className="font-[Montserrat] font-bold text-[10 px] uppercase tracking-[0.5%]">Watch Tristan in Action</span>

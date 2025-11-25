8dad5c504cf040935a16f2adf58bb7e1d06fd74b
Punoo-Khan <111001490+Punoo-Khan@users.noreply.github.com>
2025-11-25 23:32:36 +0500

    keynotes image update

--- src/components/Keynotes/HighImage.jsx
+++ src/components/Keynotes/HighImage.jsx
@@ -1,9 +1,10 @@
-import image from "../../assets/HIGHCHOOL.svg";
+import image from "../../assets/HIGHCHOOL (4).webp";
 import { Heading } from "../Heading/heading";
 export default function High() {
   return (
     <div>
-      <Heading imgSrc="https://pub-94f7f5d00cd94150915de158d63cafdd.r2.dev/HIGHCHOOL.svg" altText={"Text img"} />
+      <Heading imgSrc={image} altText={"Text img"} />
     </div>
   );
 }
+//https://pub-94f7f5d00cd94150915de158d63cafdd.r2.dev/HIGHCHOOL.svg
